083ab659ff9bcbe81d5e6b2b2ff60811fb00eeea
Allan Moura <allan.moura16@gmail.com>
2020-02-19 17:22:30 -0300

    Changing error logs

--- src/routes.js
+++ src/routes.js
@@ -8,12 +8,12 @@ const npcController = require('./controller/NpcController');
 
 //Criar um usuario e gerar o token de login
 routes.post('/user/signup', [
-    check("username", "Insira um nome de usuário válido").not().isEmpty(),
-    check("password", "Insira uma senha válida").isLength({min: 6})
+    check("username", "Insira um nome de usuário").not().isEmpty(),
+    check("password", "Insira uma senha válida, mínimo de 6 caracteres").isLength({min: 6})
 ], userController.signup);
 //Checar se username e senha são compativeis e gerar um token de login
 routes.post('/user/login', [
-    check("username", "Insira um nome de usuário válido").not().isEmpty(),
+    check("username", "Insira um nome de usuário").not().isEmpty(),
     check("password", "Insira uma senha válida").isLength({min: 6})
 ], userController.login);
 //Buscar o usuário através do token
